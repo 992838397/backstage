35bd80c33b80169e91e3ea35b7fb1bb3c3fcaf99
linziyun <992838397@qq.com>
2021-03-31 14:51:13 +0800

    登陆注册接口完成

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ const jwt = require('express-jwt');
 // app.use(jwt().unless());
 // jwt() 用于解析token，并将 token 中保存的数据 赋值给 req.user
 // unless() 约定某个接口不需要身份认证
-// server.use(jwt({
-//     secret: 'heima61', // 生成token时的 钥匙，必须统一
-//     algorithms: ['HS256'] // 必填，加密算法，无需了解
-// }).unless({
-//     path: ['/api/add', '/my/register'] // 除了这两个接口，其他都需要认证
-// }));
+server.use(jwt({
+    secret: 'gz61', // 生成token时的 钥匙，必须统一
+    algorithms: ['HS256'] // 必填，加密算法，无需了解
+}).unless({
+    path: ['/api/register', '/api/login'] // 除了这两个接口，其他都需要认证
+}));
 
 // 通过路由中间键,加载不同的路由
 const apiRouter = require('./router/api_router')
@@ -28,12 +28,12 @@ server.use('/api', apiRouter);
 // server.use('myarticle', myArticleRouter);
 
 // // 错误处理
-// server.use((err, req, res, next) => {
-//     if (err.name === 'UnauthorizedError') {
-//         // res.status(401).send('invalid token...');
-//         res.status(401).send({ status: 1, message: '身份认证失败！' });
-//     }
-// });
+server.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+        // res.status(401).send('invalid token...');
+        res.status(401).send({ status: 1, message: '身份认证失败！' });
+    }
+});
 
 
 server.use(cors());
--- router/api_router.js
+++ router/api_router.js
@@ -3,13 +3,85 @@ const express = require('express');
 // 创建一个路由
 const router = express.Router();
 // 引入自定义模块SQL服务器启动
-const sql = require('../util/sql')
+const conn = require('../util/sql')
+const jwt = require('jsonwebtoken')
 
-router.get('/add', (req, res) => {
-    console.log(req.query);
-    res.json('ok')
+
+// POST请求普通键值对接收
+router.use(express.urlencoded())
+
+
+// 登陆注册接口
+
+// 1.注册接口
+router.post('/register', (req, res) => {
+    // 接收参数
+    // console.log(req.body);
+    const { username, password } = req.body;
+    // 注册的用户名不能相同,则需要查询一次,如果查到,则说明用户名存,如果没有查询到,说明用户注册成功
+    const sqlStrselect = `select username from users where username="${username}"`;
+    // const sqlStrselect = `select username from user where username="${userName}"`
+    // 调用query()
+    conn.query(sqlStrselect, (err, result) => {
+        if (err) {
+            res.json({ message: '服务器失败', status: 500 })
+            return
+        }
+        console.log(result);
+        // console.log("这是result长度", result.length);
+        if (result.length > 0) {
+            res.json({ message: '注册失败,用户名已存在', status: 1 })
+            return
+        }
+        // 用户名没有被占用 继续执行
+        //SQL语句拼接
+        const sqlStr = `insert into users(username,password) values("${username}","${password}")`
+        // 调用SQL的方法 query()
+        conn.query(sqlStr, (err, result) => {
+            // 请求失败则执行下面语句
+            if (err) {
+                res.json({ message: "注册失败", status: 1 })
+                return
+            }
+            // 执行成功
+            res.json({ message: '注册成功', status: 0 })
+        })
+
+
+
+    })
+
+
+
+
+    // 根据不同的SQL语句返回不同的值
+    // res.json('ok')
 })
 
+// 2.登陆接口
+router.post('/login', (req, res) => {
+    // 接收参数
+    console.log(req.body);
+    const { username, password } = req.body
+    // 拼接SQL
+    const sqlstr = `select username,password from users where  username="${username}" and password="${password}"`;
+    conn.query(sqlstr, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.json({ message: "服务器错误", status: 1 })
+        }
+        if (result.length > 0) {
+            // 未来接口要用到的token,
+            const token = "Bearer " + jwt.sign({ name: 'userName' }, 'gz61', { expiresIn: 2 * 60 * 60 * 60 });
+            res.json({ message: "登陆成功", status: 0, token })
+        } else {
+            res.json({ message: '登陆失败,用户名或密码错误', status: 201 })
+        }
+    })
+
+    // 根据不同的情况返回不同的值
+
+})
 // 导出
 module.exports = router;
 
